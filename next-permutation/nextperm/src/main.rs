185370652af3887dfec255403dd6a1853b214c8d
William Ehlhardt <williamehlhardt@gmail.com>
2017-02-27 03:12:23 -0500

    Give insertion point tests better names.

--- next-permutation/nextperm/src/main.rs
+++ next-permutation/nextperm/src/main.rs
@@ -13,11 +13,11 @@ fn find_insertion_point(v: Vec<i32>, val: i32, start: usize) -> usize {
 }
 
 #[cfg(test)]
-mod tests {
+mod find_insertion_point {
     use super::find_insertion_point;
     
     #[test]
-    fn simple_insertion() {
+    fn simple() {
         assert_eq!(find_insertion_point(vec![2, 3, 1], 2, 1), 1);
         assert_eq!(find_insertion_point(vec![1,3,2], 1, 1), 2);
         assert_eq!(find_insertion_point(vec![1,4,3,2], 1, 1), 3);
