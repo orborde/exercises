4469d2f960ef00493a71dbdc2c363e7d53fb777f
William Ehlhardt <williamehlhardt@gmail.com>
2017-02-27 03:59:09 -0500

    Implement permute(), with a little bit of testing.

--- next-permutation/nextperm/src/main.rs
+++ next-permutation/nextperm/src/main.rs
@@ -30,6 +30,44 @@ mod find_insertion_point {
     }
 }
 
+fn permute(arr: &mut Vec<i32>) {
+    // The right-hand subarray is in descending order. Figure out how
+    // far left that subarray goes.
+    let mut subarray_start = arr.len() - 1;
+    while subarray_start > 0 &&
+        arr[subarray_start-1] >= arr[subarray_start] {
+            subarray_start -= 1;
+        }
+
+    if subarray_start != 0 {
+        let ins_idx = subarray_start - 1;
+        let ins_val = arr[ins_idx];
+        let ins_point = find_insertion_point(
+            arr, ins_val, subarray_start);
+        arr.swap(ins_point, ins_idx);
+    }
+
+    let subarr = &mut arr[subarray_start..];
+    subarr.reverse();
+}
+
+#[cfg(test)]
+mod permute {
+    use super::permute;
+
+    fn permute_wrapper(mut arr: Vec<i32>) -> Vec<i32> {
+        permute(&mut arr);
+        return arr
+    }
+
+    #[test]
+    fn simple() {
+        assert_eq!(permute_wrapper(vec![1,2]), vec![2,1]);
+        assert_eq!(permute_wrapper(vec![2,1]), vec![1,2]);
+    }
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
