8a6c3a229ae01376e06fc36127b416df9e35e332
William Ehlhardt <williamehlhardt@gmail.com>
2017-03-06 19:20:57 -0800

    Start working on the warden puzzle. No real progress yet.

---
+++ chessboard-warden-puzzle/try3.py
@@ -0,0 +1,19 @@
+def checksum(bits):
+    """
+    >>> checksum([1, 0, 1])
+    2
+    """
+    sm = 0
+    for bit, val in zip(bits, range(len(bits))):
+        sm += val * bit
+    return sm % len(bits)
+
+import itertools
+def checkstrat():
+    pass
+
+if __name__ == '__main__':
+    import doctest
+    fails, _ = doctest.testmod()
+    assert fails == 0
+
