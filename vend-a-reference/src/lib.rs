0e351f0250649303bc34051201b0431408d31bb4
William Ehlhardt <williamehlhardt@gmail.com>
2017-03-10 19:00:10 -0800

    Create a Shifter that vends clones of its internal state for benchmarking purposes.

--- vend-a-reference/src/lib.rs
+++ vend-a-reference/src/lib.rs
@@ -47,9 +47,51 @@ impl Shifter {
     }
 }
 
+struct CopyShifter {
+    arr:   Vec<i32>,
+    state: ShifterState
+}
+
+impl CopyShifter {
+    fn new(val: i32, len: usize) -> CopyShifter {
+        CopyShifter {
+            arr: vec![0; len],
+            state: ShifterState::New{val:val}
+        }
+    }
+}
+
+impl Iterator for CopyShifter {
+    type Item = Vec<i32>;
+
+    fn next(&mut self) -> Option<Vec<i32>> {
+        match self.state {
+            ShifterState::New {val} => {
+                self.arr[0] = val;
+                self.state = ShifterState::Partial{idx:0};
+                Some(self.arr.clone())
+            },
+            ShifterState::Partial{idx} => {
+                let val = self.arr[idx];
+                self.arr[idx] = 0;
+                let nidx = idx + 1;
+                if nidx == self.arr.len() - 1 {
+                    self.state = ShifterState::Complete;
+                } else {
+                    self.state = ShifterState::Partial{idx:nidx};
+                }
+                self.arr[nidx] = val;
+                Some(self.arr.clone())
+            },
+            ShifterState::Complete => None
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::Shifter;
+    use super::CopyShifter;
 
     #[test]
     fn simple() {
@@ -57,6 +99,15 @@ mod tests {
         assert_eq!(*shifter.next().unwrap(), vec![10, 0, 0]);
         assert_eq!(*shifter.next().unwrap(), vec![0, 10, 0]);
         assert_eq!(*shifter.next().unwrap(), vec![0, 0, 10]);
+        assert!(shifter.next().is_none());
+    }
+
+    #[test]
+    fn copysimple() {
+        let mut shifter = CopyShifter::new(10, 3);
+        assert_eq!(shifter.next().unwrap(), vec![10, 0, 0]);
+        assert_eq!(shifter.next().unwrap(), vec![0, 10, 0]);
+        assert_eq!(shifter.next().unwrap(), vec![0, 0, 10]);
         assert!(shifter.next().is_none());
     }
 }
