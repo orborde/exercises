223b283f01e58887f22409c61f9c46f2781b3251
William Ehlhardt <williamehlhardt@gmail.com>
2017-03-21 21:34:26 -0700

    Reorganize tests to live under a tests:: submodule.

--- next-permutation/rust-impl/src/lib.rs
+++ next-permutation/rust-impl/src/lib.rs
@@ -16,24 +16,6 @@ fn find_insertion_point(v: &Vec<usize>, val: usize, start: usize) -> usize {
     return end
 }
 
-#[cfg(test)]
-mod find_insertion_point {
-    use super::find_insertion_point;
-    
-    #[test]
-    fn simple() {
-        assert_eq!(find_insertion_point(&vec![2, 3, 1], 2, 1), 1);
-        assert_eq!(find_insertion_point(&vec![1,3,2], 1, 1), 2);
-        assert_eq!(find_insertion_point(&vec![1,4,3,2], 1, 1), 3);
-    }
-
-    #[test]
-    #[should_panic]
-    fn bad_start() {
-        find_insertion_point(&vec![2, 3, 1], 2, 0);
-    }
-}
-
 fn permute(arr: &mut Vec<usize>) {
     // The right-hand subarray is in descending order. Figure out how
     // far left that subarray goes.
@@ -56,49 +38,68 @@ fn permute(arr: &mut Vec<usize>) {
 }
 
 #[cfg(test)]
-mod permute {
-    use super::permute;
+mod tests {
+    mod find_insertion_point {
+        use super::super::find_insertion_point;
 
-    fn permute_wrapper(mut arr: Vec<usize>) -> Vec<usize> {
-        permute(&mut arr);
-        return arr
-    }
-
-    #[test]
-    fn simple() {
-        assert_eq!(permute_wrapper(vec![1,2]), vec![2,1]);
-        assert_eq!(permute_wrapper(vec![2,1]), vec![1,2]);
-    }
-
-    use permutohedron::LexicalPermutation;
-    // Permutohedron actually already implements this, so I'll go
-    // ahead and test against that. Yes, this is goofy - I want to
-    // check whether I got it right!
-    fn test_cycle(start: Vec<usize>) {
-        let mut cur = start.clone();
-        let mut lib_cur = cur.clone();
-        loop {
-            assert_eq!(cur, lib_cur);
-
-            permute(&mut cur);
-            if !lib_cur.next_permutation() {
-                break
-            }
+        #[test]
+        fn simple() {
+            assert_eq!(find_insertion_point(&vec![2, 3, 1], 2, 1), 1);
+            assert_eq!(find_insertion_point(&vec![1,3,2], 1, 1), 2);
+            assert_eq!(find_insertion_point(&vec![1,4,3,2], 1, 1), 3);
         }
-        // Okay, permute() should have looped around to the beginning.
-        assert_eq!(cur, start);
+
+        #[test]
+        #[should_panic]
+        fn bad_start() {
+            find_insertion_point(&vec![2, 3, 1], 2, 0);
+        }
     }
 
-    use std::iter::FromIterator;
-    use stepper::stepper;
-    #[test]
-    fn vs_lib() {
-        for l in 0..6 {
-            let values = Vec::from_iter(0..l);
-            for idxv in stepper(0, l, l) {
-                println!("{:?}", idxv);
-                let arr = Vec::from_iter(idxv.iter().map(|i| values[i.clone()]));
-                test_cycle(arr);
+    mod permute {
+        use super::super::permute;
+
+        fn permute_wrapper(mut arr: Vec<usize>) -> Vec<usize> {
+            permute(&mut arr);
+            return arr
+        }
+
+        #[test]
+        fn simple() {
+            assert_eq!(permute_wrapper(vec![1,2]), vec![2,1]);
+            assert_eq!(permute_wrapper(vec![2,1]), vec![1,2]);
+        }
+
+        use permutohedron::LexicalPermutation;
+        // Permutohedron actually already implements this, so I'll go
+        // ahead and test against that. Yes, this is goofy - I want to
+        // check whether I got it right!
+        fn test_cycle(start: Vec<usize>) {
+            let mut cur = start.clone();
+            let mut lib_cur = cur.clone();
+            loop {
+                assert_eq!(cur, lib_cur);
+
+                permute(&mut cur);
+                if !lib_cur.next_permutation() {
+                    break
+                }
+            }
+            // Okay, permute() should have looped around to the beginning.
+            assert_eq!(cur, start);
+        }
+
+        use std::iter::FromIterator;
+        use stepper::stepper;
+        #[test]
+        fn vs_lib() {
+            for l in 0..6 {
+                let values = Vec::from_iter(0..l);
+                for idxv in stepper(0, l, l) {
+                    println!("{:?}", idxv);
+                    let arr = Vec::from_iter(idxv.iter().map(|i| values[i.clone()]));
+                    test_cycle(arr);
+                }
             }
         }
     }
