057295529a1bd7148a35a90b36d5cb48fb5da6bd
William Ehlhardt <williamehlhardt@gmail.com>
2018-03-21 21:10:05 -0700

    More constraints

--- dishonored-jindosh-riddle/solve.py
+++ dishonored-jindosh-riddle/solve.py
@@ -79,8 +79,8 @@ def family_of(atom):
 class binary_constraint:
     def __init__(self, x, y):
         valid(x,y)
-        self._x = x
-        self._y = y
+        self.x = x
+        self.y = y
 
 def foodle(world,x,y):
     for other_y in FAMILIES[family_of(y)]:
@@ -93,29 +93,84 @@ def foodle(world,x,y):
 
 class equiv(binary_constraint):
     def could_be(self, world):
-        return (foodle(world, self._x, self._y) and
-                foodle(world, self._y, self._x))
+        return (foodle(world, self.x, self.y) and
+                foodle(world, self.y, self.x))
 
     def satisfied(self, world):
-        return is_related(world, self._x, self._y)
+        return is_related(world, self.x, self.y)
 
 def find_position(world, x):
     if family_of(x) == position:
         return x
+    for pos in FAMILIES[position]:
+        if is_related(world,x,pos):
+            return pos
+    return None
 
 class is_next_to(binary_constraint):
     def could_be(self, world):
         return True  # fuck it
     def satisfied(self, world):
-        pass
+        xp = find_position(world, self.x)
+        yp = find_position(world, self.y)
+
+        if xp is None or yp is None:
+            return False
+
+        return abs(yp-xp) == 1
 
 class is_left_of(binary_constraint):
     def could_be(self, world):
-        pass
-    def satisfied(self, world):
-        pass
+        xp = find_position(world, self.x)
+        yp = find_position(world, self.y)
 
-CONSTRAINTS = [
+        if xp is None or yp is None:
+            return True
+
+        return self.satisfied(self, world)
+
+    def satisfied(self, world):
+        xp = find_position(world, self.x)
+        yp = find_position(world, self.y)
+
+        if xp is None or yp is None:
+            return False
+
+        return xp+1 == yp
+
+def relation_count(world, x, family):
+    ct = 0
+    for y in FAMILIES[family]:
+        if is_related(world, x, y):
+            ct += 1
+    return ct
+
+class uniquely_paired:
+    def __init__(self, x):
+        self.x = x
+        self.xfam = family_of(self.x)
+
+    def could_be(self, world):
+        for family in FAMILIES:
+            if family == self.xfam:
+                continue
+
+            if relation_count(world, x, family) > 1:
+                return False
+
+        return True
+
+    def satisfied(self, world):
+        for family in FAMILIES:
+            if family == self.xfam:
+                continue
+
+            if relation_count(world, x, family) != 1:
+                return False
+
+        return True
+
+BASE_CONSTRAINTS = [
     equiv(marcolla, red),
     equiv(winslow, 0),
     is_next_to(winslow, green),
@@ -133,3 +188,9 @@ CONSTRAINTS = [
     equiv(natsiou, dabokva),
 ]
 
+print len(BASE_CONSTRAINTS), 'base constraints'
+
+GENERATED_CONSTRAINTS = []
+for atom in ALL_ATOMS:
+    GENERATED_CONSTRAINTS.append(uniquely_paired(atom))
+print len(GENERATED_CONSTRAINTS), 'generated constraints'
