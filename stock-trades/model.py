74c5982c7211f557d96c773733d09230b859913b
William Ehlhardt <williamehlhardt@gmail.com>
2018-09-10 22:45:41 -0700

    Model some stock trades against a random walk price model.
    I'm sure that this code totally works and has zero bugs given that I slammed it out over the course of like an hour with no unit tests.

---
+++ stock-trades/model.py
@@ -0,0 +1,66 @@
+import collections
+import itertools
+from fractions import Fraction
+
+steps = 5
+start_price = Fraction(10)
+stocks = 20
+
+def gen_price_history():
+    for moves in itertools.product([-1, 1], repeat=steps):
+        amount = start_price
+        prices = []
+        for mov in moves:
+            amount += mov
+            prices.append(amount)
+        assert len(prices) == steps
+        yield prices
+
+
+def sell_at_once():
+    histo = collections.defaultdict(int)
+    for prices in gen_price_history():
+        for pt in range(steps):
+            sale = prices[pt] * stocks
+            histo[sale] += 1
+    return histo
+
+def sell_sharded():
+    schedule = []
+    for _ in range(steps - 1):
+        schedule.append(int(stocks / steps))
+    schedule.append(stocks - sum(schedule))
+
+    histo = collections.defaultdict(int)
+    for prices in gen_price_history():
+        sale = 0
+        for amt,price in zip(schedule,prices):
+            sale += amt*price
+        histo[sale] += 1
+    return histo
+
+def mean(histo):
+    sm,ct = 0,0
+    for val,wt in histo.items():
+        sm += wt*val
+        ct += wt
+    return Fraction(sm) / Fraction(ct)
+
+def variance(histo):
+    u = mean(histo)
+
+    sm,ct = 0,0
+    for wt,val in histo.items():
+        sm += wt * ( (val-u) ** 2 )
+        ct += wt
+
+    return Fraction(sm) / Fraction(ct)
+
+
+for f in [sell_at_once, sell_sharded]:
+    print f.__name__,
+    histo = f()
+    print int(mean(histo)), int(variance(histo)),
+    for k in sorted(histo.keys()):
+        print '{}:{}'.format(k, histo[k]),
+    print
